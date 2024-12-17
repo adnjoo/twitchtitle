6594554c2976e4507ff4edebdea12abbf8b375cd
Andrew Njoo <adnjoo@gmail.com>
2024-12-16 20:14:36 -0600

    route

---
+++ src/app/api/twitch/oauth/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const url = "https://id.twitch.tv/oauth2/token";
+
+  // Load environment variables
+  const clientId = process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID;
+  const clientSecret = process.env.TWITCH_CLIENT_SECRET;
+
+  if (!clientId || !clientSecret) {
+    return new Response(
+      JSON.stringify({ error: "Missing environment variables" }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
+
+  const params = new URLSearchParams({
+    client_id: clientId,
+    client_secret: clientSecret,
+    grant_type: "client_credentials",
+  });
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: params,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Twitch token endpoint error: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: error.message || "Something went wrong" }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
+}
