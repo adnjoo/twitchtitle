0fca5c453541701c4428d5b2872e8e3e23ebfe2b
Andrew Njoo <adnjoo@gmail.com>
2024-12-17 22:24:12 -0600

    cs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className='row-start-3 flex flex-wrap items-center justify-center gap-6'>
       <a
         className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-        href='https://github.com/adnjoo/twitch-title'
+        href='https://github.com/adnjoo/twitchtitle'
         target='_blank'
         rel='noopener noreferrer'
       >
